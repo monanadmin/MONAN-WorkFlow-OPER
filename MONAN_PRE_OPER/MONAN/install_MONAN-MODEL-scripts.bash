2a821fd2a9d1493d69f10583406cfe87e8e70268
carlosrenatosouza2 <carlos.renato.souza@gmail.com>
2025-05-07 14:54:03 -0300

    #release/0.1.0: adjustments on ecfs scripts for new versions of scripts_CD-CT and MONAN.

--- MONAN_PRE_OPER/MONAN/install_MONAN-MODEL-scripts.bash
+++ MONAN_PRE_OPER/MONAN/install_MONAN-MODEL-scripts.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SCRIPTSCDCT_VERSION=1.2.0
-MONAN_VERSION=1.3.1-rc
+SCRIPTSCDCT_VERSION=release/1.3.0
+MONAN_VERSION=release/1.4.0-rc
 CONVERT_MPAS_VERSION=1.1.0
 
 if [ ! -d "scripts_CD-CT" ]; then
@@ -17,4 +17,4 @@ else
    cd scripts_CD-CT/scripts
    ./1.install_monan.bash https://github.com/monanadmin/MONAN-Model.git ${MONAN_VERSION} ${CONVERT_MPAS_VERSION}
 fi
-cat ../execs/VERSION.txt
+
